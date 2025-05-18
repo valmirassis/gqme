d356a0203a984ddc2bf3d19f6423b15162c7dd7c
valmirassis <assis@rediscover.com.br>
2025-05-17 21:39:07 -0300

    Add files via upload

--- pages/Informações.py
+++ pages/Informações.py
@@ -36,20 +36,22 @@ with st.expander("📂 Modo: Baseado em Tema", expanded=True):
 with st.expander("📄 Modo: Baseado em Arquivo PDF", expanded=True):
     st.markdown("""
     - O modelo utilizará o conteúdo **presente no arquivo** para gerar a questão.
-    - No campo **“Sobre o que a questão deve tratar?”** v ocê pode (opcionalmente) 
-    informar um assunto específico dentro do arquivo, como:
-        - `"Evolução dos sistemas de informação"` (caso esteja contido dentro de um artigo maior)
-    - 
+    -  Nesta opção ainda é possível informar um assunto presente no arquivo para que a questão seja gerada com base
+         naquele assunto, por exemplo, um artigo sobre fundamentos dos sistemas de informação, e no artigo há um
+         subtópico sobre a evolução dos sistemas de informações, assim, caso queira uma questão especificamente sobre
+         esse tópico você deve informar isso no campo **'Sobre o que a questão deve tratar?'**
+         
 
     :blue-background[**Importante:** O modelo pode gerar conteúdo irrelevante (alucinação) se o texto não fornecer base suficiente.]
 
-    🔹 **Recomendação:** use arquivos entre **2 e 20 páginas**.  
-    🔹 **Observação:** páginas com menos de **300 caracteres** são **descartadas pelo sistema**.
+    🔹 **Recomendação:** use arquivos entre **2 e 20 páginas**.  \n
+    🔹 **Observação:** páginas com menos de **300 caracteres** são **descartadas pelo sistema**. \n
+    🔹 O sistema lê apenas os textos presentes no arquivo, sem considerar o conteúdo gráfico.\n
     """)
 
 st.divider()
 
-st.markdown("## 🧠 Estruturas de Questão Suportadas")
+st.markdown("## 🧠 Estruturas de questões suportadas")
 
 st.markdown("""
 ### ✅ Múltipla Escolha Simples
@@ -68,4 +70,7 @@ st.markdown("""
 - 1 **asserção** e 1 **razão**.
 - 5 alternativas (**a–e**) sobre a relação entre as proposições.
 - O sistema indicará a alternativa correta e justificará as incorretas.
+
+### ✅ Número de questões
+- É possível gerar 1, 2 ou 3 questões por solicitação, porém, na geração por arquivos recomenda-se a geração de apenas 1 questão.
 """)
